563067ecfaf57b5dbf1e55b14a0e0e95f2a15d27
Dave Dailey <dave@davedailey.com>
2017-02-14 10:44:47 -0600

    fuck me, forgot to commit weeks ago
     Changes to be committed:

---
+++ InAppPurchases/InAppPurchases/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  InAppPurchases
+//
+//  Created by DAVID DAILEY on 1/18/17.
+//  Copyright © 2017 davedailey. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: 157.0/255.0, green: 157.0/255.0, blue: 157.0/255.0, alpha: 0.5).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width:0.0, height: 2.0)
+    }
+
+}
